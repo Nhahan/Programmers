3f0457016ef400fd1706fdfab6f6f6224060f983
Nhahan <kisy324@naver.com>
2021-09-19 02:10:44 +0900

    약수의 합 답 / 효율성 더 올린 답

--- level_1/17. 약수의 합.py
+++ level_1/17. 약수의 합.py
@@ -1,6 +1,6 @@
 def solution(n):
-    div = []
-    for i in range(1, n + 1):
+    div = [n]
+    for i in range(1, n//2 + 1):
         if n % i == 0:
             div.append(i)
     return sum(div)

e8ed74e4148ab2741eb8f5235e8e17856d5057aa
Nhahan <kisy324@naver.com>
2021-09-20 03:04:10 +0900

    두 정수 사이의 합 답 / 더 좋은 답

--- level_1/28. 두 정수 사이의 합.py
+++ level_1/28. 두 정수 사이의 합.py
@@ -1,9 +1,5 @@
 def solution(a, b):
-    answer = 0
-    if a <= b:
-        for i in range(a, b + 1):
-            answer = answer + i
-    elif a > b:
-        for i in range(b, a + 1):
-            answer = answer + i
-    return answer
+    if a > b:
+        a, b = b, a
+
+    return sum(range(a, b + 1))

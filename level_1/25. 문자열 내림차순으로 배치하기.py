ad67e75cfad15d42cc2a25800a88bfde2c03ee05
Nhahan <kisy324@naver.com>
2021-09-20 02:37:19 +0900

    문자열 내림차순으로 배치하기 답 / 더 좋은 답

--- level_1/25. 문자열 내림차순으로 배치하기.py
+++ level_1/25. 문자열 내림차순으로 배치하기.py
@@ -1,3 +1,2 @@
 def solution(string):
-    s_list = sorted([ord(s) for s in list(string)], reverse=True)
-    return "".join([chr(s) for s in s_list])
+    return "".join(sorted(string, reverse=True))

97d988aa8e6854e13929260cce0802672e12b60e
Nhahan <sun@codebrick.co>
2021-11-07 19:50:43 +0900

    실패율 답

--- level_1/46. 실패율.py
+++ level_1/46. 실패율.py
@@ -5,12 +5,12 @@ def solution(N, stages):
         total = len([x for x in stages if x >= i])
         fail = stages.count(i)
         if total == 0: break
-        failure[i - 1] = fail/total
+        failure[i - 1] = fail / total
 
     li = []
     for i, v in enumerate(failure):
         li.append((v, i + 1))
-    result = sorted(li, key = lambda x : (x[0], -x[1]), reverse=True)
+    result = sorted(li, key=lambda x: (x[0], -x[1]), reverse=True)
     result = [i[1] for i in result]
 
     return result

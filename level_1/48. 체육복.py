55a2b9e2ba3ad312de672622bf7d67a57b1b2ab4
Nhahan <sun@codebrick.co>
2021-11-28 20:15:35 +0900

    체육복 오답3

--- level_1/48. 체육복.py
+++ level_1/48. 체육복.py
@@ -1,21 +1,33 @@
 def solution(n, lost, reserve):
-    students = [i for i in range(1, n + 1) if i not in lost]
-
+    answer = n - len(lost)
+    left = [i for i in range(1, n + 1)]
     for i in reserve:
-        if i - 1 >= 1 and i - 1 in lost:
-            reserve.remove(i)
-            lost.remove(i - 1)
-            students.append(i - 1)
-        elif i in lost:
-            reserve.remove(i)
-            lost.remove(i)
-            students.remove(i)
-        elif i + 1 in lost:
-            reserve.remove(i)
-            lost.remove(i + 1)
-            students.append(i + 1)
-
-    return len(students)
+        left.append(i)
+    for i in lost:
+        left.remove(i)
+    left.sort()
+    repetition = []
+    for i in set(left):
+        if left.count(i) == 2:
+            repetition.append(i)
+    for i in repetition:
+        if left.count(i) == 2:
+            if i == 1:
+                if i + 1 not in left:
+                    left.append(i + 1)
+                    answer += 1
+            elif i != n:
+                if i - 1 not in left:
+                    left.append(i - 1)
+                    answer += 1
+                elif i + 1 not in left:
+                    left.append(i + 1)
+                    answer += 1
+            else:
+                if i - 1 not in left:
+                    left.append(i - 1)
+                    answer += 1
+    return answer
 
 
 print(solution(5, [2, 4], [1, 3, 5]))

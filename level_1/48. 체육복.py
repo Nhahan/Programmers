30f1e6e0c63704442832cb56da1afd18b2914d1e
Nhahan <sun@codebrick.co>
2021-12-04 23:21:56 +0900

    도대체 왜 오답..!?

--- level_1/48. 체육복.py
+++ level_1/48. 체육복.py
@@ -1,36 +1,12 @@
 def solution(n, lost, reserve):
-    answer = n - len(lost)
-    left = [i for i in range(1, n + 1)]
-    left.sort()
-    reserve.sort()
-
-    for i in reserve:
-        left.append(i)
-    for i in lost:
-        left.remove(i)
-    left.sort()
-    repetition = []
-    for i in set(left):
-        if left.count(i) == 2:
-            repetition.append(i)
-    for i in repetition:
-        if left.count(i) == 2:
-            if i == 1:
-                if i + 1 not in left:
-                    left.append(i + 1)
-                    answer += 1
-            elif i != n:
-                if i - 1 not in left:
-                    left.append(i - 1)
-                    answer += 1
-                elif i + 1 not in left:
-                    left.append(i + 1)
-                    answer += 1
-            else:
-                if i - 1 not in left:
-                    left.append(i - 1)
-                    answer += 1
-    return answer
+    set_reserve = set(reserve) - set(lost)
+    set_lost = set(lost) - set(reserve)
+    for i in set_reserve:
+        if i - 1 in set_lost:
+            set_lost.remove(i - 1)
+        elif i + i in set_lost:
+            set_lost.remove(i + 1)
+    return n - len(set_lost)
 
 
 print(solution(5, [2, 4], [1, 3, 5]))

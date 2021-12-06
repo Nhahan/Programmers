8663e23387e2a4865d6ae873125797e36a12fd98
Nhahan <sun@codebrick.co>
2021-12-06 23:12:58 +0900

    소수 만들기 답

--- level_1/49. 소수 만들기.py
+++ level_1/49. 소수 만들기.py
@@ -4,8 +4,9 @@ from itertools import combinations
 
 def solution(nums):
     possible_list = [sum(i) for i in combinations(nums, 3)]
+    print(possible_list)
     primes = is_prime_num(max(possible_list))
-    combs = set([i for i in possible_list if i in primes])
+    combs = ([i for i in possible_list if i in primes])
     return len(combs)
 
 
